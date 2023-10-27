8d1ce415e2f94c9270f22c3f914bb6ec5933eb3c
sadministrator <marcothewebdev@gmail.com>
2023-10-26 22:04:40 -0600

    fix ping

--- src/daemon/global_rpc_protocol.rs
+++ src/daemon/global_rpc_protocol.rs
@@ -1,25 +1,14 @@
 use async_trait::async_trait;
-use bytes::Bytes;
 use nanorpc::nanorpc_derive;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-use super::{
-    global_rpc_client::GLOBAL_RPC_DOCK,
-    socket::{Endpoint, Socket},
-    DaemonContext,
-};
+use super::DaemonContext;
 
 #[nanorpc_derive]
 #[async_trait]
 pub trait GlobalRpcProtocol {
-    async fn ping(&self, endpoint: Endpoint, ping_value: Bytes) -> Result<(), PingError>;
-}
-
-#[derive(Error, Serialize, Deserialize, Debug)]
-pub enum PingError {
-    #[error("unable to ping")]
-    Ping,
+    async fn ping(&self, i: u64) -> u64;
 }
 
 pub struct GlobalRpcImpl {
@@ -28,13 +17,7 @@ pub struct GlobalRpcImpl {
 
 #[async_trait]
 impl GlobalRpcProtocol for GlobalRpcImpl {
-    async fn ping(&self, endpoint: Endpoint, ping_value: Bytes) -> Result<(), PingError> {
-        let socket = Socket::bind(self.ctx.clone(), None, Some(GLOBAL_RPC_DOCK));
-        socket
-            .send_to(ping_value, endpoint)
-            .await
-            .map_err(|_| PingError::Ping)?;
-
-        Ok(())
+    async fn ping(&self, i: u64) -> u64 {
+        i
     }
 }
