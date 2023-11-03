cbdb6049ee7f3b2094e0be4475b2ddd58032906d
antidmg <antidmg@proton.me>
2023-11-03 18:22:48 -0500

    unified socket API with inner enum

--- src/daemon/socket.rs
+++ src/daemon/socket.rs
@@ -1,11 +1,52 @@
+use bytes::Bytes;
+use earendil_crypt::{Fingerprint, IdentitySecret};
 use earendil_packet::Dock;
 
-use super::DaemonContext;
+use super::{
+    haven_socket::HavenSocket,
+    n2r_socket::{Endpoint, N2rSocket},
+    DaemonContext,
+};
 
-pub struct Socket {}
+pub struct Socket {
+    ctx: DaemonContext,
+    inner: InnerSocket,
+}
 
 impl Socket {
-    pub fn bind(&self, ctx: DaemonContext, dock: Option<Dock>) -> Socket {
-        todo!()
+    pub fn bind(
+        &self,
+        ctx: DaemonContext,
+        dock: Option<Dock>,
+        anon_id: Option<IdentitySecret>,
+        identity_sk: Option<IdentitySecret>,
+        rendezvous_point: Option<Fingerprint>,
+    ) -> Socket {
+        let inner = if let Some(isk) = identity_sk {
+            InnerSocket::Haven(HavenSocket::bind(ctx.clone(), isk, dock, rendezvous_point))
+        } else {
+            InnerSocket::N2R(N2rSocket::bind(ctx.clone(), anon_id, dock))
+        };
+
+        Socket { ctx, inner }
     }
+
+    pub async fn send_to(&self, body: Bytes, endpoint: Endpoint) -> anyhow::Result<()> {
+        match &self.inner {
+            InnerSocket::N2R(s) => s.send_to(body, endpoint).await,
+            InnerSocket::Haven(s) => s.send_to(body, endpoint).await,
+        }
+    }
+
+    pub async fn recv_from(&self) -> anyhow::Result<(Bytes, Endpoint)> {
+        match &self.inner {
+            InnerSocket::N2R(s) => s.recv_from().await,
+            InnerSocket::Haven(s) => s.recv_from().await,
+        }
+    }
+}
+
+enum InnerSocket {
+    Haven(HavenSocket),
+    N2R(N2rSocket),
 }
