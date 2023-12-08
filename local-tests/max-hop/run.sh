0fc619abb6d021582a78f07ea1deb0570e077f77
nullchinchilla <nullchinchilla@pm.me>
2023-12-08 13:05:27 -0500

    Add named pipe for server communication and streamline transfer command in max-hop test script

--- local-tests/max-hop/run.sh
+++ local-tests/max-hop/run.sh
@@ -20,6 +20,11 @@ SOSISTAB2_NO_SLEEP=1 earendil daemon --config heidi.yaml > /dev/null 2>&1 &
 SOSISTAB2_NO_SLEEP=1 earendil daemon --config ivan.yaml > /dev/null 2>&1 &
 SOSISTAB2_NO_SLEEP=1 earendil daemon --config judy.yaml > /dev/null 2>&1 &
 
+# echo server
+rm .fifo
+mknod -m 777 .fifo p
+cat .fifo | nc -lk 127.0.0.1 $to_port > .fifo &
+
 echo "sleeping for ${gossip_time}s to allow nodes to gossip..."
 for i in $(seq 1 $gossip_time); do
     sleep 1
@@ -31,10 +36,8 @@ earendil control graph-dump
 echo "judy graph:"
 earendil control --connect 127.0.0.1:10010 graph-dump
 
-nc -lk 127.0.0.1 $to_port > /dev/null 2>&1 &
-
-echo "end-to-end maximum hop transfer speed:"
-yes | head -n $transfer_size | pv -s $(($transfer_size * 2)) | nc -w 5 127.0.0.1 $forward_to
+echo "starting transfer..."
+cat /dev/zero | head -c $transfer_size | nc 127.0.0.1 $forward_to | pv > /dev/null 
 
 killall earendil
 killall nc
