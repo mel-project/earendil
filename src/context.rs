5b1a2d9ce85ff92b11a2b57a7e38c54d4fb5814f
sadministrator <marco@melproject.org>
2024-04-03 18:06:05 -0600

    persist client id

--- src/context.rs
+++ src/context.rs
@@ -3,8 +3,13 @@ use earendil_packet::crypt::DhSecret;
 use earendil_topology::RelayGraph;
 
 use parking_lot::RwLock;
+use stdcode::{deserialize, StdcodeSerializeExt};
 
-use crate::{config::ConfigFile, db::db_read, debts::Debts};
+use crate::{
+    config::ConfigFile,
+    db::{db_read, db_write},
+    debts::Debts,
+};
 
 pub type DaemonContext = anyctx::AnyCtx<ConfigFile>;
 pub type CtxField<T> = fn(&DaemonContext) -> T;
@@ -38,7 +43,6 @@ pub static RELAY_GRAPH: CtxField<RwLock<RelayGraph>> = |ctx| {
 };
 
 pub static DEBTS: CtxField<Debts> = |ctx| {
-    let ctx = ctx.clone();
     smol::future::block_on(async move {
         match db_read(&ctx, "debts").await {
             Ok(Some(debts)) => {
@@ -59,7 +63,30 @@ pub static DEBTS: CtxField<Debts> = |ctx| {
     })
 };
 
-pub static MY_CLIENT_ID: CtxField<ClientId> = |_| {
-    let rando = rand::random::<u64>();
-    rando / 100000 * 100000
+pub static MY_CLIENT_ID: CtxField<ClientId> = |ctx| {
+    smol::future::block_on(async {
+        match db_read(ctx, "client_id").await {
+            Ok(Some(id)) => {
+                let client_id = deserialize(&id).unwrap_or(generate_client_id(ctx).await);
+                tracing::debug!("retrieved client id {client_id}");
+                client_id
+            }
+            Ok(None) => generate_client_id(ctx).await,
+            Err(e) => {
+                tracing::warn!("error retrieving client id: {e}");
+                generate_client_id(ctx).await
+            }
+        }
+    })
 };
+
+async fn generate_client_id(ctx: &DaemonContext) -> ClientId {
+    let id = rand::random::<u64>() / 100000 * 100000;
+    tracing::debug!("generated new client id: {id}");
+
+    if let Err(e) = db_write(ctx, "client_id", id.stdcode()).await {
+        tracing::warn!("error saving client id: {e}");
+    }
+
+    id
+}
