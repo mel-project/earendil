663737be43a7f52d994814e2640cf6b211af6dda
sadministrator <marco@melproject.org>
2024-07-03 18:39:55 -0600

    use get_coin() to verify tx confirmation and retransmit tx intermittently

--- src/link_node/payment_system/onchain.rs
+++ src/link_node/payment_system/onchain.rs
@@ -2,8 +2,6 @@ use anyhow::Context;
 use async_trait::async_trait;
 use base32::Alphabet;
 use bytes::Bytes;
-use futures::StreamExt;
-use melprot::CoinChange;
 use melstructs::{Address, BlockHeight, CoinData, CoinID, CoinValue, Denom, NetID};
 use melwallet::{PrepareTxArgs, StdEd25519Signer};
 use smol::Timer;
@@ -51,6 +49,7 @@ impl OnChain {
 #[async_trait]
 impl PaymentSystem for OnChain {
     async fn pay(&self, my_id: NodeId, to: &str, amount: u64, ott: &str) -> anyhow::Result<String> {
+        tracing::debug!("initiating on-chain payment of {amount} micromel to {to} with code {ott}");
         let mut wallet = self.wallet.clone();
         // we place (my_id, ott) into `additional_data` so payee can identify our payment
         let payment_output = CoinData {
@@ -63,8 +62,10 @@ impl PaymentSystem for OnChain {
 
         // sync wallet before preparing tx
         if let Some(owned_coins) = snapshot.get_coins(wallet.address).await? {
+            tracing::debug!("syncing wallet...");
             wallet.full_reset(snapshot.current_header().height, owned_coins)?
         }
+
         let fee_multiplier = snapshot.current_header().fee_multiplier;
         let tx = wallet.prepare_tx(
             PrepareTxArgs {
@@ -83,56 +84,25 @@ impl PaymentSystem for OnChain {
             index: 0,
         };
 
-        snapshot.get_raw().send_tx(tx).await??;
+        snapshot.get_raw().send_tx(tx.clone()).await??;
+        tracing::debug!("payment transaction sent");
 
         // wait for the tx to confim on the blockchain
+        let mut current_height = wallet.height;
         loop {
-            let current_height = wallet.height;
-            let latest_height = self
-                .mel_client
-                .latest_snapshot()
-                .await?
-                .current_header()
-                .height;
-            let height_diff = latest_height.0 - current_height.0;
-
-            if height_diff > 0 {
-                let mut stream = self
-                    .mel_client
-                    .stream_snapshots(wallet.height + BlockHeight(1))
-                    .take(height_diff as usize)
-                    .map(|snapshot| async {
-                        let ccs = snapshot.get_coin_changes(self.wallet.address).await?;
-                        let mut new_coins = vec![];
-                        let mut spent_coins = vec![];
-                        for cc in ccs {
-                            match cc {
-                                CoinChange::Add(id) => {
-                                    if let Some(data_height) = snapshot.get_coin(id).await? {
-                                        new_coins.push((id, data_height.coin_data));
-                                    }
-                                }
-                                CoinChange::Delete(id, _) => {
-                                    spent_coins.push(id);
-                                }
-                            }
-                        }
-                        anyhow::Ok((snapshot, new_coins, spent_coins))
-                    })
-                    .buffered(10)
-                    .boxed();
-
-                while let Some(item) = stream.next().await {
-                    let (_snapshot, new_coins, _spent_coins) = item?;
-                    for (id, _cdh) in &new_coins {
-                        if id == &coin_id {
-                            // return payment coin ID as proof
-                            return Ok(coin_id.to_string());
-                        }
-                    }
-                }
-            }
             Timer::after(Duration::from_secs(5)).await;
+            let latest_snapshot = self.mel_client.latest_snapshot().await?;
+            let latest_height = latest_snapshot.current_header().height;
+
+            if current_height < latest_height {
+                if let Some(coin) = latest_snapshot.get_coin(coin_id).await? {
+                    // return payment coin ID as proof
+                    return Ok(coin_id.to_string());
+                }
+                current_height = latest_height;
+            }
+            // resend tx in case it got dropped
+            let _ = latest_snapshot.get_raw().send_tx(tx.clone()).await;
         }
     }
 
@@ -148,9 +118,8 @@ impl PaymentSystem for OnChain {
         if let Some(coin) = coin_data {
             let (id, ott): (NodeId, String) =
                 stdcode::deserialize(&coin.coin_data.additional_data)?;
-            println!("we are about to verify");
             if id == from && coin.coin_data.value == CoinValue(amount.into()) {
-                println!("it worked");
+                tracing::debug!("payment verified");
                 return Ok(Some(ott));
             }
         }
