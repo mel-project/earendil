71dbec0cb1df5473791465a2614eccad6b89b26a
thisbefruit <thisbefruit@protonmail.com>
2024-07-05 18:44:02 -0400

    update deploy scripts to deploy to big new server

--- deploy-testing.sh
+++ deploy-testing.sh
@@ -4,9 +4,5 @@ cargo build  --release --locked --target x86_64-unknown-linux-musl
 mv ./target/x86_64-unknown-linux-musl/release/earendil ./target/x86_64-unknown-linux-musl/release/earendil-testing
 
 # rsync binary to example-relay-free & restart
-rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil-testing root@45.33.109.28:/usr/local/bin/
-ssh root@45.33.109.28 'systemctl restart earendil-testing'
-
-# rsync binary to example-relay-paid & restart
-rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil-testing root@172.233.162.12:/usr/local/bin/
-ssh root@172.233.162.12 'systemctl restart earendil-testing'
+rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil-testing root@62.210.93.59:/usr/local/bin/
+ssh root@62.210.93.59 'systemctl restart earendil-testing'
--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 
 cargo build  --release --locked --target x86_64-unknown-linux-musl
 
-# rsync binary to example-relay-free & restart
-# rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil root@45.33.109.28:/usr/local/bin/
-# ssh root@45.33.109.28 'systemctl restart earendil'
-
-# rsync binary to example-relay-paid & restart
-rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil root@172.233.162.12:/usr/local/bin/
-ssh root@172.233.162.12 'systemctl restart earendil'
+# rsync binary to bootstrap node & restart
+rsync -az --info=progress2 ./target/x86_64-unknown-linux-musl/release/earendil root@62.210.93.59:/usr/local/bin/
+ssh root@62.210.93.59 'systemctl restart earendil'
